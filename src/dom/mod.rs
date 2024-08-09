d63d191ba1624eb3696b6df54f928fba0e206e19
Sachin <s474996633@gmail.com>
2024-08-10 01:27:10 +0530

    oops

---
+++ src/dom/mod.rs
@@ -0,0 +1,22 @@
+#[derive(Debug, PartialEq, Clone)]
+pub enum Node {
+    Element(String),
+    Text(String),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Element {
+    pub tag_name: String,
+    pub attributes: Vec<(String, String)>,
+    pub children: Vec<Node>,
+}
+
+impl Element {
+    pub fn new(tag_name: String) -> Self {
+        Element {
+            tag_name,
+            attributes: Vec::new(),
+            children: Vec::new(),            
+        }
+    }   
+}
