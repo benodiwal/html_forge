cfbbfb5be6786302003db202435ddc1000a33ef7
Sachin <s474996633@gmail.com>
2024-08-10 02:01:51 +0530

    parsers utils

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -18,4 +18,35 @@ impl Parser {
     fn parse_node(&mut self) -> Result<Node, ParseError> {   
         todo!()
     }
+
+    fn consume_whitespace(&mut self) {
+        self.consume_while(char::is_whitespace);
+    }
+
+    fn consume_while<F>(&mut self, test: F) -> String
+    where
+       F: Fn(char) -> bool,
+    {
+        let mut result = String::new();
+        while !self.eof() && test(self.next_char()) {
+            result.push(self.consume_char());
+        }
+        result
+    }
+
+    fn consume_char(&mut self) -> char {
+        let mut iter = self.input[self.position..].char_indices();
+        let (_, cur_char) = iter.next().unwrap();
+        let (next_pos, _) = iter.next().unwrap_or((1, ' '));
+        self.position += next_pos;
+        cur_char
+    }
+
+    fn next_char(&self) -> char {
+        self.input[self.position..].chars().next().unwrap()
+    }
+
+    fn eof(&self) -> bool {
+        self.position >= self.input.len()
+    }
 }
